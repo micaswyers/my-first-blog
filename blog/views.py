fa4e98d170da7312a48959e03c68ac4cf7b84506
Mica Swyers <micaswyers@gmail.com>
2017-01-07 23:05:57 -0800

    Add view/create comment functionality

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import (
     render,
 )
 from django.utils import timezone
-from .forms import PostForm
+from .forms import CommentForm, PostForm
 from .models import Post
 
 # Create your views here.
@@ -68,3 +68,17 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
