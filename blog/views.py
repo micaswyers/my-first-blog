9a82c57a485eda9d097ea32de7ae393dcca623f4
Mica Swyers <micaswyers@gmail.com>
2017-01-07 18:22:19 -0800

    Add publish functionality

--- blog/views.py
+++ blog/views.py
@@ -52,3 +52,8 @@ def post_draft_list(request):
         'created_date',
     )
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
